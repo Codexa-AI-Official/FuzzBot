b52f314c725d8ddadc7aa7d6e3e2a6905f7e7fc1
Codexa <gundlsha000@isd284.com>
2026-08-12 16:11:58 -0500

    Add showDocs function to display documentation message

---
+++ static/script.js
@@ -0,0 +1,11 @@
+```javascript
+function showDocs() {
+
+    const message =
+        document.getElementById("docs-message");
+
+    message.innerHTML =
+        "📖 Documentation is coming soon!";
+
+}
+```
